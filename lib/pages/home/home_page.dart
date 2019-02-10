f54cebb74825d31dcde4a4c8187f3d392aa63db3
matgrz1993 <matgrz1993@gmail.com>
2019-02-10 17:00:02 +0100

    Add home page image

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:business_mobile_app/utils/widgets/appbars.dart';
 import 'package:flutter/material.dart';
 
 import 'offer/offer_page.dart';
@@ -18,34 +19,25 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext aContext) {
-    final appBar = new PreferredSize(
-        preferredSize: Size.fromHeight(150.0),
-        child: Container(
-          decoration: new BoxDecoration(
-              image: new DecorationImage(
-            image: new AssetImage("assets/images/app_bar_image.png"),
-            fit: BoxFit.cover,
-          )),
-          child: AppBar(
-            title: new Text(HomePage.Title, style: new TextStyle(color: gGoldColor)),
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-          ),
-        ));
-
     final body = new Center(
       child: Column(
         children: <Widget>[
           new Padding(padding: EdgeInsets.all(40)),
           new Container(
-            child: new Image(image: AssetImage("assets/images/logo_white.png")),
+            child: new Image(
+              image: AssetImage("assets/images/logo_white.png"),
+              width: 150,
+            ),
           ),
           new Padding(padding: EdgeInsets.all(40)),
           new Text(
             "Lider wyjazdów w Polsce",
             textAlign: TextAlign.center,
             style: new TextStyle(
-                color: gGoldColor, fontSize: 20.0, fontWeight: FontWeight.bold),
+                fontFamily: 'Amithen',
+                color: gBrownColor,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold),
           ),
         ],
       ),
@@ -60,7 +52,7 @@ class _HomePageState extends State<HomePage> {
 
     return new Scaffold(
         backgroundColor: Colors.black,
-        appBar: appBar,
+        appBar: fGetDefaultAppBar(),
         drawer: drawer,
         body: body);
   }
--- lib/utils/widgets/appbars.dart
+++ lib/utils/widgets/appbars.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import '../fonts.dart';
 
-AppBar fGetDefaultAppBar(String title) {
-  return AppBar(title: Text(title, style: TextStyle(color: gGoldColor)));
+AppBar fGetDefaultAppBar([String title = ""]) {
+  return AppBar(
+      iconTheme: IconThemeData(
+        color: Colors.black,
+      ),
+      backgroundColor: gBrownColor,
+      title: Text(title, style: TextStyle(color: gGoldColor)));
 }
